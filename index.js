31cea3f1caf5382c6441f315bde54aa5cffdec69
Isa Zhang <isazhang15@163.com>
2016-09-26 20:56:57 +0800

    try

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var app = require('express')(),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    wechat = require('./lib/wechat.js'),
+    conf = require('./lib/config.js');
 
 //xml那两个暂时不需要，那个用来解析
 
@@ -9,6 +11,9 @@ app.use(bodyParser.urlencoded({
 //用object的方式传输，所以是{}
 
 app.use('/wxapi',require('./lib/routers/wxapi.js'));
+
+wechat(conf.wechat);//初始化，接受配置
+wechat.createMenu(require('./lib/menu.json'));
 
 app.listen(8008,function(err){
   console.log('listening at 8008...');

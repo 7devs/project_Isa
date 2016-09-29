14465f50fff5710cf6fffbfa84db823db4ec2f96
Isa Zhang <isazhang15@163.com>
2016-09-29 20:30:24 +0800

    template

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = require('express')(),
     bodyParser = require('body-parser'),
-    wechat = require('./lib/wechat.js'),
-    conf = require('./lib/config.js');
+    wechat = require('./lib/wechat.js'),//这个被引入了，因为在这里index要调用它
+    conf = require('./lib/config.js');//同样被引入了，也要用
 
 //xml那两个暂时不需要，那个用来解析
 
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({
 
 app.use('/wxapi',require('./lib/routers/wxapi.js'));
 
-wechat(conf.wechat);//初始化，接受配置
+wechat(conf.wechat);//初始化，接受配置。在config.js里面，定义了wechat到底是什么。
 wechat.createMenu(require('./lib/menu.json'));
 
 app.listen(8008,function(err){
